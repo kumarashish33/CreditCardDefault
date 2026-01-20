995414b5ca5f24ff8c292e1624f812d9a9805867
kumarashish33 <kumar3ashish32@gmail.com>
2026-01-20 20:51:13 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.set_page_config(
+    page_title="Credit Card Default Prediction",
+    layout="wide"
+)
+
+st.title("Credit Card Default Prediction")
+st.write("Machine Learning Classification Models Comparison")
